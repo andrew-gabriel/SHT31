38976b699dcba7b8885acfbd6a3a177098dbec1f
Yadwinder Singh <ryker1990@gmail.com>
2016-04-19 14:07:40 +0530

    Update SHT31.py

--- Python/SHT31.py
+++ Python/SHT31.py
@@ -11,23 +11,22 @@ import time
 bus = smbus.SMBus(1)
 
 # SHT31 address, 0x44(68)
-# Send measurement command
-#		0x2C06(11270)	High repeatability measurement
-data = [0x2C06]
-bus.write_i2c_block_data(0x44, 0x00, 0x2C06)
+# Send measurement command, 0x2C(44)
+#		0x06(06)	High repeatability measurement
+bus.write_i2c_block_data(0x44, 0x2C, [0x06])
 
 time.sleep(0.5)
 
-# SHT31 address, 0x44(68)
+# SHT30 address, 0x44(68)
 # Read data back from 0x00(00), 6 bytes
 # Temp MSB, Temp LSB, Temp CRC, Humididty MSB, Humidity LSB, Humidity CRC
 data = bus.read_i2c_block_data(0x44, 0x00, 6)
 
 # Convert the data
-cTemp = data[0] * 256 + data[1]
+temp = data[0] * 256 + data[1]
 cTemp = -45 + (175 * temp / 65535.0)
 fTemp = -49 + (315 * temp / 65535.0)
-humididty = 100 * (data[3] * 256 + data[4]) / 65535.0
+humidity = 100 * (data[3] * 256 + data[4]) / 65535.0
 
 # Output data to screen
 print "Temperature in Celsius is : %.2f C" %cTemp
